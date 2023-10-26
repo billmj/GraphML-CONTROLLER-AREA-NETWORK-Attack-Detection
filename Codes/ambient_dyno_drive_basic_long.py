4899370abf5f087d9cd5fc50647d02270a096394
Marfo, William <wmarfo@miners.utep.edu>
2023-10-26 01:27:31 +0000

    Update ambient_dyno_drive_basic_long.py

--- Codes/ambient_dyno_drive_basic_long.py
+++ Codes/ambient_dyno_drive_basic_long.py
@@ -1,15 +1,15 @@
-# ambient_dyno_drive_basic_long.py
 from imports import *
 
 from data_processing import make_can_df, generate_graphs_from_data, process_dataframe
-from embedding_generation import generate_node_embeddings, combine_embeddings_with_attributes, compute_average_embeddings, create_dataframe_from_embeddings, display_combined_embeddings
+from embedding_generation import (generate_node_embeddings, combine_embeddings_with_attributes, 
+                                 compute_average_embeddings, create_dataframe_from_embeddings, display_combined_embeddings)
 from utils import load_mapped_capture, find_node_with_highest_signals, save_dataframe_to_csv
-
 
 def main():
     parser = argparse.ArgumentParser(description='Process CAN log data and generate node embeddings.')
     parser.add_argument('--window-size', type=float, required=True, help='Size of the window for slicing data.')
     parser.add_argument('--offset', type=float, required=True, help='Offset for slicing data.')
+    parser.add_argument('--pkl-folder', type=str, required=True, help='Folder name for the .pkl file.')
     parser.add_argument('log_filepath', type=str, help='Path to the CAN log file.')
     args = parser.parse_args()
 
@@ -23,8 +23,8 @@ def main():
     # Generate node embeddings
     all_node_embeddings = generate_node_embeddings(all_graphs)
 
-    # Load mapped_capture for attribute extraction
-    mapped_capture = load_mapped_capture()
+    # Load mapped_capture for attribute extraction using the specified pkl folder
+    mapped_capture = load_mapped_capture(args.pkl_folder)
 
     # Combine the embeddings with attributes
     all_combined_node_embeddings = combine_embeddings_with_attributes(all_graphs, all_node_embeddings, args.window_size, mapped_capture)
@@ -36,7 +36,7 @@ def main():
     # Find the node with the highest number of signals
     node, highest_signal_count = find_node_with_highest_signals(mapped_capture)
     print(f"Node with Highest Signals (Node ID, Signal Count): ({node}, {highest_signal_count})")
-    
+
     # Compute average embeddings
     concatenated_embeddings = compute_average_embeddings(all_combined_node_embeddings, highest_signal_count)
 
@@ -44,18 +44,15 @@ def main():
     print("Number of graphs processed:", len(concatenated_embeddings))
     print("Keys in concatenated_embeddings:", list(concatenated_embeddings.keys()))
 
-    
-
     # Use the function to create the DataFrame
     df_benign = create_dataframe_from_embeddings(concatenated_embeddings)
-    
+
     # Display the first few rows of the DataFrame
     print(df_benign.head())
 
     # Display the last few rows of the DataFrame
     print(df_benign.iloc[-5:])
-    
-    
+
     # Save the DataFrame to a CSV file
     save_dataframe_to_csv(df_benign, args.window_size, args.offset, args.log_filepath)
 
@@ -65,4 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
