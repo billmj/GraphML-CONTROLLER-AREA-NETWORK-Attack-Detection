eb5b0bdf40ad3a87c8475cbf88817998c4ee6686
Marfo, William <wmarfo@miners.utep.edu>
2023-10-26 01:27:49 +0000

    Update process_all_files.py

--- Codes/process_all_files.py
+++ Codes/process_all_files.py
@@ -1,4 +1,3 @@
-#process_all_files.py
 import subprocess
 import os
 
@@ -6,27 +5,27 @@ def main():
     # The directory where our log files are located
     log_directory = "C:\\Users\\willi\\OneDrive\\Desktop\\Research\\oak_ridge_in_vehicle\\road\\ambient\\"
 
-    # List of log files we want to process
-    log_files = [
-        "ambient_dyno_drive_basic_short.log",
-        "ambient_dyno_drive_benign_anomaly.log",
-        "ambient_dyno_drive_extended_long.log",
-        "ambient_dyno_drive_extended_short.log",
-        "ambient_dyno_drive_radio_infotainment.log",
-        "ambient_dyno_drive_winter.log",
-        "ambient_dyno_exercise_all_bits.log",
-        "ambient_dyno_idle_radio_infotainment.log",
-        "ambient_dyno_reverse.log"
-    ]
+    # Dictionary mapping log files to their respective pkl folders
+    log_to_pkl = {
+        "ambient_dyno_drive_basic_short.log": "road_ambient_dyno_drive_basic_short_020822_030640",
+        "ambient_dyno_drive_benign_anomaly.log": "road_ambient_dyno_drive_benign_anomaly_030804_082640",
+        "ambient_dyno_drive_extended_long.log": "road_ambient_dyno_drive_extended_long_040716_134640",
+        "ambient_dyno_drive_extended_short.log": "road_ambient_dyno_drive_extended_short_021215_195320",
+        "ambient_dyno_drive_radio_infotainment.log": "road_ambient_dyno_drive_radio_infotainment_041109_063320",
+        "ambient_dyno_drive_winter.log": "road_ambient_dyno_drive_winter_030410_144000",
+        "ambient_dyno_exercise_all_bits.log": "road_ambient_dyno_exercise_all_bits_030410_144000",
+        "ambient_dyno_idle_radio_infotainment.log": "road_ambient_dyno_idle_radio_infotainment_030410_144000",
+        "ambient_dyno_reverse.log": "road_ambient_dyno_reverse_040322_190000"
+    }
 
     # Loop through each log file and run the processing script
-    for log_file in log_files:
+    for log_file, pkl_folder in log_to_pkl.items():
         log_filepath = os.path.join(log_directory, log_file)
         
         # Printing the name of the dataset being processed
         print(f"Processing {log_file}...")
         
-        subprocess.run(["python", "ambient_dyno_drive_basic_long.py", "--window-size", "10", "--offset", "10", log_filepath])
+        subprocess.run(["python", "ambient_dyno_drive_basic_long.py", "--window-size", "10", "--offset", "10", "--pkl-folder", pkl_folder, log_filepath])
 
 if __name__ == "__main__":
     main()
